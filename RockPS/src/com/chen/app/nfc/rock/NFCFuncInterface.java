de5ec7d13b585766ab238899f85957cff212cf52
Chen <tianclch@gmail.com>
2012-07-13 02:55:32 -0500

    add nfc interface class

---
+++ RockPS/src/com/chen/app/nfc/rock/NFCFuncInterface.java
@@ -0,0 +1,14 @@
+package com.chen.app.nfc.rock;
+
+/**
+ *   This interface is used for nfc function calls
+ *   normally we will use Android Beam function call
+ *   in case we have a ealier Android version
+ *   we use the old nfc calls
+ * 
+ *  @author chen
+ *
+ */
+public interface NFCFuncInterface {
+
+}
